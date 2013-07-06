3fd88eaf0f14890ff9921897f9b244450ccecef2
infinitec123 <infinitecsolutions@gmail.com>
2013-07-06 10:03:16 +0530

    Extra

--- app/controllers/puzzles_controller.rb
+++ app/controllers/puzzles_controller.rb
@@ -31,7 +31,7 @@ class PuzzlesController < ApplicationController
 
   	def index
   		
-  		@puzzles = Puzzle.paginate(:per_page => 5, :page => params[:page]).order('created_at DESC')
+  		@puzzles = Puzzle.paginate(:per_page => 5, :page => params[:page])
 
   		respond_to do |format|
       		format.html # index.html.erb
